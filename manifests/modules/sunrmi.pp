411dd971c9a4d4ff445053ba252508ca849220c3
Keith Burdis <keith@burdis.org>
2012-11-30 16:46:40 +0000

    Temporary hack to add sun/rmi/server.

--- manifests/modules/sunrmi.pp
+++ manifests/modules/sunrmi.pp
@@ -3,7 +3,6 @@ define jboss::sunrmi (
   $user,
   $group,
 ) {
-  $module = 'sunrmi'
   $dir    = "${install_dir}/modules/sun/jdk/main"
   # Use XSLT to add the sun/rmi/server line:
 
@@ -20,4 +19,22 @@ define jboss::sunrmi (
 #                ...
 #                <path name="META-INF/services"/>
 #            </paths>
+  $module = 'sunjdk'
+  exec { "mkdir-${module}-${version}":
+    command => "/bin/mkdir -p ${dir}",
+    require => File[$install_dir],
+  }
+  file { $dir:
+    ensure  => directory,
+    owner   => $user,
+    group   => $group,
+    require => Exec["mkdir-${module}-${version}"],
+  }
+  file { "${dir}/module.xml":
+    ensure  => present,
+    owner   => $user,
+    group   => $group,
+    content => template("jboss/jboss-as/modules/${module}-module.xml"),
+    require => File[$dir],
+  }
 }
